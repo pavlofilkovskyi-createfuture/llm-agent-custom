62116b7befb3fd55249d4a9ec8c4c2b3f12ee339
pavlofilkovskyi-createfuture <pavlo.filkovskyi@createfuture.com>
2025-11-04 09:31:22 +0000

    add filesystem mcp

--- main.py
+++ main.py
@@ -12,33 +12,40 @@ load_dotenv()  # Load from .env file
 
 app = Flask(__name__)
 
+# https://github.com/modelcontextprotocol/servers/tree/main/src/git
 git_server_params = StdioServerParameters(
     command = sys.executable,
     args = ["-m", "mcp_server_git"],  # MCP Server
     env = None,  # Optional environment variables
 )
 
-# TODO: add filesystem server parameters
+# https://github.com/modelcontextprotocol/servers/tree/main/src/filesystem
+filesystem_server_params = StdioServerParameters(
+    command = "npx",
+    args = ["-y", "@modelcontextprotocol/server-filesystem", "~/Projects"],
+    env = None,
+)
 
 client = Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 async def send_request(prompt:str):
     async with \
-        stdio_client(git_server_params) as (git_read, git_write):
-        # TODO: add filesystem server
+        stdio_client(git_server_params) as (git_read, git_write), \
+        stdio_client(filesystem_server_params) as (fs_read, fs_write):
         
         async with \
-            ClientSession(git_read, git_write) as git_session:
-            # TODO: add filesystem session
-
+            ClientSession(git_read, git_write) as git_session, \
+            ClientSession(fs_read, fs_write) as fs_session:
+            
             await git_session.initialize()
+            await fs_session.initialize()
 
             response = await client.aio.models.generate_content(
                 model = "gemini-2.5-flash",
                 contents = prompt,
                 config = genai.types.GenerateContentConfig(
                     temperature = 0,
-                    tools = [git_session],
+                    tools = [git_session, fs_session],
                 ),
             )
             print(response.text)
